7e982935435fcbe1175a6c9c8e0f7b387ddbe42b
Bugingo Twesiime Will <willbugingo@gmail.com>
2026-02-13 06:16:50 +0300

    Fix GeneticEngine evolve implementation

--- evolution/genetic_engine.py
+++ evolution/genetic_engine.py
@@ -1,1 +1,7 @@
-class GeneticEngine: pass
+﻿class GeneticEngine:
+    def evolve(self, performance, crisis_feedback):
+        return {
+            "updated": False,
+            "performance": performance,
+            "crisis_feedback": crisis_feedback,
+        }
